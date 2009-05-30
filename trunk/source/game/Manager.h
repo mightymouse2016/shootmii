47af3c599e5b6b68866eaa696522f9e4099b78e1
denisgeorge666 <denisgeorge666@c8eafc54-4d23-11de-9e51-a92ed582648b>
2009-05-30 17:20:22 +0000

    gestion manager

---
+++ trunk/source/game/Manager.h
@@ -0,0 +1,35 @@
+#ifndef MANAGER_H_
+#define MANAGER_H_
+
+#include "../ShootMii.h"
+
+namespace shootmii {
+
+  const int N_ROWS(52);
+  const int N_COLS(80);
+  const int PLAYER_OFFSET(5);
+
+  class Manager {
+    private:
+      Player* player1;
+      Player* player2;
+      Terrain* terrain;
+    public:
+      Manager(string nick_p1 = "Player 1", string nick_p2 = "Player 2");
+      ~Manager();
+      // Accesseurs
+      Player* getPlayer1();
+      Player* getPlayer2();
+      // Autres méthodes
+      Player* opponent(Player*);
+      void initPlayerPosition(Player* player, int offSet = 0) const;
+      void moveLeft(Player*);
+      void moveRight(Player*);
+      void draw();
+      void show();
+      void dealEvent(const u32, const u32);
+  };
+
+}
+
+#endif /*MANAGER_H_*/
