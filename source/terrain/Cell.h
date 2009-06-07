efe21e3b3f8c5681b3bb14f9ab36ee0cb289024b
denisgeorge666 <denisgeorge666@c8eafc54-4d23-11de-9e51-a92ed582648b>
2009-06-07 20:46:41 +0000

    gestion des jauges de puissance + gestion collisions

--- source/terrain/Cell.h
+++ source/terrain/Cell.h
@@ -3,26 +3,29 @@
 
 #include "../ShootMii.h"
 
-namespace shootmii{
-    
-    const int CELL_SIZE(8);
+namespace shootmii {
 
-    class Cell{
-        protected:
-          int rowIndex;
-          int colIndex;
-          int screenX;
-          int screenY;
-        public:
-            //Constructeur
-          Cell(const int _rowIndex = 0, const int _colIndex = 0);
-          // Accesseurs
-          void setCoords(const int _rowIndex, const int _colIndex);
-          int getRowIndex() const;
-          int getColIndex() const;
-          int getscreenX() const;
-          int getscreenY() const;
-    };
+  const int CELL_SIZE(8);
+
+  class Cell {
+    protected:
+      int rowIndex;
+      int colIndex;
+      int screenX;
+      int screenY;
+    public:
+      //Constructeur
+      Cell(const int _rowIndex = 0, const int _colIndex = 0);
+      // Accesseurs
+      void setCoords(const int _rowIndex, const int _colIndex);
+      int getRowIndex() const;
+      int getColIndex() const;
+      int getScreenX() const;
+      int getScreenY() const;
+      void setScreenX(int);
+      void setScreenY(int);
+      virtual bool cellIntersect(const Cell*) const;
+  };
 }
 
 #endif /*CELL_H_*/
