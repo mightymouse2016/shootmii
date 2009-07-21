07a3e7ddff39b459e81974ae20e6dd17c0fa6f9d
denisgeorge666 <denisgeorge666@c8eafc54-4d23-11de-9e51-a92ed582648b>
2009-07-21 15:44:09 +0000

    gestion du soleil

---
+++ source/terrain/Sun.cpp
@@ -0,0 +1,25 @@
+#include "../ShootMii.h"
+
+namespace shootmii{
+
+Sun::Sun() :
+  Rectangle(
+      SUN_WIDTH,
+      SUN_HEIGHT,
+      SCREEN_WIDTH/2,
+      (CENTER_TERRAIN+AMPLITUDE_TERRAIN+SUN_HEIGHT/2)*N_ROWS*TERRAIN_CELL_WIDTH/100,
+      (SCREEN_WIDTH - SUN_WIDTH)/2,
+      -PI/2,
+      0,
+      0,
+      App::imageBank->get(TXT_SUN))
+{
+  
+}
+
+void Sun::compute(){
+  if (angle >= 0) angle = -PI;
+  else angle += SUN_SLOW;
+}
+
+}
---
+++ source/terrain/Sun.h
@@ -0,0 +1,21 @@
+#ifndef SUN_H_
+#define SUN_H_
+
+#include "../ShootMii.h"
+
+namespace shootmii{
+
+const float SUN_LIGHT_INFLUENCE(0.9); // 0 -> 1
+const float SUN_SLOW(0.001);
+const int SUN_WIDTH(64);
+const int SUN_HEIGHT(64);
+  
+class Sun : public Rectangle{
+public:
+	Sun();
+	void compute();
+};
+
+}
+
+#endif /*SUN_H_*/
