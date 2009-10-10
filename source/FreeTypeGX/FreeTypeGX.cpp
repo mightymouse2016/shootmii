c241584a6e86a84b67cccb4e392255acf4704df0
denisgeorge666 <denisgeorge666@c8eafc54-4d23-11de-9e51-a92ed582648b>
2009-10-10 21:28:12 +0000

    Ajout des dependances au niveau des headers et des libs

--- source/FreeTypeGX/FreeTypeGX.cpp
+++ source/FreeTypeGX/FreeTypeGX.cpp
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with FreeTypeGX.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include <string>
 #include "FreeTypeGX.h"
 
 /**
