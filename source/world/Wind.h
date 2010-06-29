18fcc98ca4820e85f58881e9d865148b431bc3c1
Altarfinch <Altarfinch@c8eafc54-4d23-11de-9e51-a92ed582648b>
2010-06-29 11:43:10 +0000

    - Changement de direction et d'intensité du vent aléatoirement au cours de la partie

--- source/world/Wind.h
+++ source/world/Wind.h
@@ -5,18 +5,24 @@
 
 namespace shootmii {
 
+const int WIND_TURNOVER_PROBABILITY(60*60); // 1fois toutes les minutes
+
 const float WIND_INFLUENCE_ON_AMMO(10); // 0->100
 const float WIND_INFLUENCE_ON_FRONT_CLOUDS(1); // 0->100
 const float WIND_INFLUENCE_ON_BACK_CLOUDS(.5); // 0->100
 
 class Wind {
 private:
-	float windSpeedLeft; // Deux variables car plus simple pour les jauges ...
+	bool windModificationFlag;
+	bool turnover; // false : gauche, true : droite
 	float windSpeedRight;
+	float windSpeedLeft; // Deux variables car plus simple pour les jauges ...
 public:
+	Wind();
 	float* getPWindSpeedLeft();
 	float* getPWindSpeedRight();
 	float getWindSpeed() const;
+	void compute();
 	void init();
 };
 
